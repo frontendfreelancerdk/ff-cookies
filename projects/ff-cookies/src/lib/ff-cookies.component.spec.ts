532edce64d44a280e0fe3cc01d2c20a36471cda7
Marat Tsaryk <marat@frontend-freelancer.com>
2019-02-06 19:44:01 +0200

    .fix u-t

--- projects/ff-cookies/src/lib/ff-cookies.component.spec.ts
+++ projects/ff-cookies/src/lib/ff-cookies.component.spec.ts
@@ -1,26 +1,26 @@
 import {async, ComponentFixture, TestBed} from '@angular/core/testing';
 import {RouterTestingModule} from '@angular/router/testing';
 
-import {FfCookiesComponent} from './ff-cookies.component';
-import {DebugElement, ElementRef} from '@angular/core';
+import {FFCookiesComponent} from './ff-cookies.component';
+import {DebugElement} from '@angular/core';
 import {By} from '@angular/platform-browser';
 
 describe('FfCookiesComponent', () => {
-  let component: FfCookiesComponent;
-  let fixture: ComponentFixture<FfCookiesComponent>;
+  let component: FFCookiesComponent;
+  let fixture: ComponentFixture<FFCookiesComponent>;
   let de: DebugElement;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       imports: [RouterTestingModule],
-      declarations: [FfCookiesComponent]
+      declarations: [FFCookiesComponent]
     })
       .compileComponents();
     document.cookie = `ff-cookies=0; 1`;
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(FfCookiesComponent);
+    fixture = TestBed.createComponent(FFCookiesComponent);
     component = fixture.componentInstance;
     de = fixture.debugElement;
     fixture.detectChanges();
