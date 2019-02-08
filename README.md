d60e0cb03f47a426ef1877a3188e6c9a5d0a303b
Marat Tsaryk <marat@frontend-freelancer.com>
2019-02-08 17:06:32 +0200

    .fix API , readme, version 0.3.0

--- README.md
+++ README.md
@@ -12,38 +12,46 @@ npm install ff-cookies --save
 ## Using
 
 #### Basic
+
+There must be present 2 methods for check and set cookies. You should make some logic for this. It let us to use  SSR and @ngx-utils/cookies
 ```html
-<ff-cookies></ff-cookies>
+<ff-cookies
+ [checkCookies]="myMethodForCheckCookies.bind(this)"
+ [setCookies]="myMethodForSetCookies.bind(this)"
+ ></ff-cookies>
 ```
 
 #### Also you can set up options:
 ```html
 <ff-cookies
+  
+ [checkCookies]="checkCookies.bind(this)"
+ [setCookies]="setCookies.bind(this)"
+ 
+ (accept)="someHandler($event)"
+ 
  link="/cookies-politic"
  linkText="cookies policy"
  agreeText="Agree"
  description="We use cookies to ensure you the best experience. By clicking around the site you accept our "
- 
- expireDays="365"
- path="/"
- 
- (onAccept)="someHandler($event)"
+
  ></ff-cookies>
 ```
 
 ### Types and default values 
 ```typescript
+  checkCookies: () => boolean;
+  setCookies: () => void; 
+  accept: (event: Event) => void;
   description: string = 'We use cookies to ensure you the best experience. By clicking around the site you accept our ';
   linkText: string = 'cookies policy';
   link: string = '';
   agreeText: string = 'Agree';
-  path: string = '/';
-  expireDays: number | string  = 365;
 ```
 
 ## Styling 
 
-You can change default styles. Just cover
+You can change default styles. That can be used to target the overlay 
 
 ```css
 .ff-cookies-wrapper{
--- projects/ff-cookies/src/lib/ff-cookies.component.ts
+++ projects/ff-cookies/src/lib/ff-cookies.component.ts
@@ -1,69 +1,35 @@
-import {Component, EventEmitter, Input, OnInit, Output, PLATFORM_ID} from '@angular/core';
+import {Component, EventEmitter, Input, OnInit, Output, ViewEncapsulation} from '@angular/core';
 import {Router} from '@angular/router';
-import {isPlatformBrowser} from '@angular/common';
 
 @Component({
   selector: 'ff-cookies',
   templateUrl: './ff-cookies.component.html',
-  styleUrls: ['./ff-cookies.component.scss']
+  styleUrls: ['./ff-cookies.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 export class FFCookiesComponent implements OnInit {
-  @Input() link: string = '';
-  @Input() linkText: string = 'cookies policy';
-  @Input() agreeText: string = 'Agree';
-  @Input() description: string = 'We use cookies to ensure you the best experience. By clicking around the site you accept our ';
-  @Input() path: string = '/';
-  @Input() expireDays: number | string = 365;
-
-  @Output() onAccept = new EventEmitter();
+  @Input() link = '';
+  @Input() linkText = 'cookies policy';
+  @Input() agreeText = 'Agree';
+  @Input() description = 'We use cookies to ensure you the best experience. By clicking around the site you accept our ';
+  @Input() checkCookies = () => false;
+  @Input() setCookies = () => {};
+  @Output() accept = new EventEmitter();
 
   flag: boolean;
 
 
   cookiesAccept() {
-    this.setCookies('ff-cookies', 'accepted', this.expireDays);
-    this.onAccept.emit(true);
+    this.setCookies();
+    this.accept.emit(true);
     this.flag = true;
-  }
-
-  private getCookies(name: string) {
-    console.log('isPlatformBrowser(PLATFORM_ID)', isPlatformBrowser(PLATFORM_ID));
-
-    if (isPlatformBrowser(PLATFORM_ID)) {
-      const cookiesArray = document.cookie.split(';');
-      const cookieName = `${name}=`;
-      let cookie: string;
-
-      for (let i = 0; i < cookiesArray.length; i++) {
-        cookie = cookiesArray[i].replace(/^\s+/g, '');
-        if (cookie.indexOf(cookieName) === 0) {
-          return cookie.substring(cookieName.length, cookie.length);
-        }
-      }
-    }
-    return '';
-  }
-
-  private setCookies(name: string, value: string, expireDays: number | string, path: string = '/') {
-    console.log('isPlatformBrowser(PLATFORM_ID)', isPlatformBrowser(PLATFORM_ID));
-
-    if (isPlatformBrowser(PLATFORM_ID)) {
-      const date = new Date();
-      if (typeof expireDays === 'string') {
-        expireDays = parseInt(expireDays, 10);
-      }
-      date.setTime(date.getTime() + expireDays * 24 * 60 * 60 * 1000);
-      const expires = `expires=${date.toUTCString()}`;
-      const cpath = `; path=${path}`;
-      document.cookie = `${name}=${value}; ${expires}${cpath}`;
-    }
   }
 
   constructor(public router: Router) {
   }
 
   ngOnInit() {
-    this.flag = this.getCookies('ff-cookies') === 'accepted';
+    this.flag = this.checkCookies();
   }
 
   onClick(e) {
