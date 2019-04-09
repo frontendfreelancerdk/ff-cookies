c07d4923b7443e97fc6861be3e885123af55767f
Marat Tsaryk <marat@frontend-freelancer.com>
2019-04-09 20:38:54 +0300

    .fix spec

--- projects/ff-cookies/src/lib/ff-cookies.component.spec.ts
+++ projects/ff-cookies/src/lib/ff-cookies.component.spec.ts
@@ -4,16 +4,26 @@ import {RouterTestingModule} from '@angular/router/testing';
 import {FFCookiesComponent} from './ff-cookies.component';
 import {DebugElement} from '@angular/core';
 import {By} from '@angular/platform-browser';
+import {BehaviorSubject} from 'rxjs';
+import {Router} from '@angular/router';
 
 describe('FfCookiesComponent', () => {
   let component: FFCookiesComponent;
   let fixture: ComponentFixture<FFCookiesComponent>;
   let de: DebugElement;
+  const routerStub = {
+    navigate: jasmine.createSpy('navigate'),
+    events: new BehaviorSubject({})
+  };
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       imports: [RouterTestingModule],
-      declarations: [FFCookiesComponent]
+      declarations: [FFCookiesComponent],
+      providers: [{
+        provide: Router,
+        useValue: routerStub
+      }]
     })
       .compileComponents();
     document.cookie = `ff-cookies=0; 1`;
@@ -42,4 +52,36 @@ describe('FfCookiesComponent', () => {
     component.cookiesAccept();
     expect(component.flag).toBeTruthy();
   });
+  it('external', async(() => {
+    component.link = 'https://medium.com';
+    fixture.detectChanges();
+    const link = fixture.debugElement.nativeElement.querySelector('a');
+    link.click();
+
+    fixture.whenStable().then(() => {
+      expect(routerStub.navigate).toHaveBeenCalledTimes(0);
+    });
+  }));
+
+  it('inner', async(() => {
+    component.link = 'test';
+    fixture.detectChanges();
+    const link = fixture.debugElement.nativeElement.querySelector('a');
+    link.click();
+
+    fixture.whenStable().then(() => {
+      expect(routerStub.navigate).toHaveBeenCalledWith(['/test']);
+    });
+  }));
+
+  it('anchor', async(() => {
+    component.link = '#test';
+    fixture.detectChanges();
+    const link = fixture.debugElement.nativeElement.querySelector('a');
+    link.click();
+
+    fixture.whenStable().then(() => {
+      expect(routerStub.navigate).toHaveBeenCalledWith(['', 'context.html'], {fragment: 'test'});
+    });
+  }));
 });
